19a93cb4d1ebd4cd97753edd9dddc330b3eaa3a4
Jed Huo <huoshiqiu@gmail.com>
2025-02-20 16:30:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "dr.jedhu0.cc": "https://registry-1.docker.io",
+  "fr.jedhu0.cc": "https://registry-1.docker.io",
   "quay.libcuda.so": "https://quay.io",
   "gcr.libcuda.so": "https://gcr.io",
   "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
